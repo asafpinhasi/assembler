420726c48ee183f9c8548fd44b8917d852504cac
asafpinhasi <87317007+asafpinhasi@users.noreply.github.com>
2022-05-01 12:31:53 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -1,3 +1,9 @@
+/* 
+This file is in charge of te first pass over the input. It uses first_pass_utils for most of the hard work.
+In general in the first pass the program checks that the input is valid and raises error messages when needed. 
+In addition it fills the lable table for use in the second pass
+*/
+
 #include "constants.h"
 #include "first_pass.h"
 
@@ -27,7 +33,7 @@ int analyze_line(char* line, FILE * file)
     if(!no_more_chars(line, &i) && line[i] != ';')  /* not an empty line or comment */
     {
 		if(!check_line_length(line, file))
-			retval = ERR;
+	    	retval = ERR;
         else if (!get_label(line, &i, label, &gotLabel))
             retval = ERR;
         else if (gotLabel && no_more_chars(line, &i)) {
